52fd3bf9fe95f4768caf7d67beb3ac1225a29536
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-04 10:26:49 -0300

    creating initial codes of the server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 const express = require("express")
 const server = express()
 
+// configure public folder
+server.use(express.static("public"))
+
 // # routes configures of the application #
 // homepage
 // req: resquisition
 // res: response
 server.get('/', (req, res) => {
-    res.send("Hello Word!")
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get('/create-point', (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
 })
 
 
