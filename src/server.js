9ae5b6c7bcf9304683e2d963a5ce56f0e09b0b76
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-04 10:05:14 -0300

    nodemon dev package install

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 const express = require("express")
 const server = express()
 
+// # routes configures of the application #
+// homepage
+// req: resquisition
+// res: response
+server.get('/', (req, res) => {
+    res.send("Hello Word!")
+})
+
+
 
 // turn on the server
 server.listen(3000)
