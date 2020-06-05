325a6812f4d658d2d4e485e5ecc6af5545dc47ea
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-05 15:26:11 -0300

    creating method to create a new register

--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ const db = require("./database/db")
 // configure public folder
 server.use(express.static("public"))
 
+// able the use of req.body
+server.use(express.urlencoded({ extended: true }))
+
 
 // use template engine
 const nunjucks = require("nunjucks")
@@ -26,9 +29,79 @@ server.get('/', (req, res) => {
     return res.render("index.html")
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
 server.get('/create-point', (req, res) => {
+
+    // query strings
+    // console.log(req.query)
+
     return res.render("create-point.html")
 })
+
+server.post('/savepoint', (req, res) => {
+    
+    // req.body
+    // console.log(req.body)
+
+    // insert data on the database
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    db.run(query, values, function(err) {
+        if (err) {
+            console.log(err)
+        }
+
+        console.log('[personal] data registered in database')
+        console.log(this)
+
+        return res.render("create-point.html", { saved: true })
+    })
+    
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 server.get('/search-results', (req, res) => {
 
